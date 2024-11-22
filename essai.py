1d30865710eca035537fd334a07c8480c2459527
Thomas SECHERAIT <thomassecherait@mac.home>
2024-11-22 13:20:25 +0100

    premier commit

---
+++ essai.py
@@ -0,0 +1,18 @@
+a = int(input("rentre: "))
+print(a+a)
+while a <= 20:
+    print(a)
+    a = a+1
+
+
+#if a == 20:
+   # print("oui")
+#else:
+#print("non")
+#while a!=3:
+ #   a = int(input("rentre un cgiffre"))
+  #  if a == 2:
+   #     print("ok")
+    #else: 
+     #   print("different")
+
